53fe6f4cec76c1427c1cfb03674f5dbf2810b78e
armaan1908-hello <armaan1908@gmail.com>
2026-08-18 20:11:53 +0700

    Update IA_Pricing_Calculating_Function.py

--- IA_Pricing_Calculating_Function.py
+++ IA_Pricing_Calculating_Function.py
@@ -9,30 +9,55 @@ def calculate_cost(silver_weight_g, silver_price_oz, wastage_percent, labor_cost
     elif quantity <= 0:
         raise ValueError("Item quantity must be greater than 0.")
     
+def calculate_cost(silver_weight_g, silver_price_oz, wastage_percent, labor_cost, stone_cost, quantity, wholesale_markup):
     silver_price_g = silver_price_oz/30
     raw_silver_price_g = silver_price_g * silver_weight_g
+    
+    if silver_weight <= 0:
+        raise ValueError("Silver weight must be greater than 0.")
+    elif silver_price_oz <= 0:
+        raise ValueError('Silver price must be realistic "greater than 0".')
+    elif quantity <= 0:
+        raise ValueError("Item quantity must be greater than 0.")
+    
+ 
+ # Data (values) for semi and auto:   
 
     if silver_weight_g <= 5:
-      casting_labor_cost = ...
-      casting_wasted_percentage =
-      handmade_labor_cost =
-      handmade_wastage_percentage = 
-      finishing_touch_cost = 
-      polishing_cost =
+        S_handmade_labor = 12
+        S_making_labor = 10
+        S_sanding_labor = 5
+        S_polishing_labor = 5
+        s_S_plating_labor = 12
+        rh_S_plating_labor = 
+        g_S_plating_labor = 
+        rs_S_plating_labor = 
+        S_e_coating_labor =
+        S_mark_up = 14
+    
     elif silver_weight_g > 5 and silver_weight_g <= 10:
-      casting_labor_cost = ...
-      casting_wasted_percentage =
-      handmade_labor_cost =
-      handmade_wastage_percentage = 
-      finishing_touch_cost = 
-      polishing_cost =
+        M_handmade_labor = 15
+        M_making_labor = 12
+        M_sanding_labor = 8
+        M_polishing_labor = 8
+        s_M_plating_labor = 15
+        rh_M_plating_labor = 
+        g_M_plating_labor = 
+        rs_M_plating_labor = 
+        M_e_coating_labor =
+        M_mark_up =11
+      
     elif silver_weight_g > 10:
-      casting_labor_cost = ...
-      casting_wasted_percentage =
-      handmade_labor_cost =
-      handmade_wastage_percentage = 
-      finishing_touch_cost = 
-      polishing_cost =
+        L_handmade_labor = 20
+        L_making_labor = 15
+        L_sanding_labor = 10
+        L_polishing_labor = 10
+        s_L_plating_labor = 18
+        rh_L_plating_labor = 
+        g_L_plating_labor = 
+        rs_L_plating_labor = 
+        L_e_coating_labor =
+        L_mark_up = 9
     
     
     
