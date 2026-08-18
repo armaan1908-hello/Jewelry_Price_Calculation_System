0f32e1148fae5dc57a8f485849179a7d2874439e
armaan1908-hello <armaan1908@gmail.com>
2026-08-18 20:30:34 +0700

    Update IA_Pricing_Calculating_Function.py

--- IA_Pricing_Calculating_Function.py
+++ IA_Pricing_Calculating_Function.py
@@ -12,41 +12,41 @@ def calculate_cost(silver_weight_g, silver_price_oz, wastage_percent, labor_cost
  
  # Data (values) for semi and auto:   
 
-    if silver_weight_g <= 5:
+    if silver_weight_g <= 4:
         S_handmade_labor = 12
         S_making_labor = 10
         S_sanding_labor = 5
         S_polishing_labor = 5
         s_S_plating_labor = 12
-        rh_S_plating_labor = 
-        g_S_plating_labor = 
-        rs_S_plating_labor = 
-        S_e_coating_labor =
-        S_mark_up = 14
+        rh_S_plating_labor = 22 
+        g_S_plating_labor = 25
+        rs_S_plating_labor = 25
+        S_e_coating_labor = 2
+        S_mark_up = 16
     
-    elif silver_weight_g > 5 and silver_weight_g <= 10:
+    elif silver_weight_g > 4 and silver_weight_g <= 8:
         M_handmade_labor = 15
         M_making_labor = 12
         M_sanding_labor = 8
         M_polishing_labor = 8
         s_M_plating_labor = 15
-        rh_M_plating_labor = 
-        g_M_plating_labor = 
-        rs_M_plating_labor = 
-        M_e_coating_labor =
-        M_mark_up =11
+        rh_M_plating_labor = 32
+        g_M_plating_labor = 35
+        rs_M_plating_labor = 35
+        M_e_coating_labor = 3
+        M_mark_up =14
       
-    elif silver_weight_g > 10:
+    elif silver_weight_g > 8:
         L_handmade_labor = 20
         L_making_labor = 15
         L_sanding_labor = 10
         L_polishing_labor = 10
         s_L_plating_labor = 18
-        rh_L_plating_labor = 
-        g_L_plating_labor = 
-        rs_L_plating_labor = 
-        L_e_coating_labor =
-        L_mark_up = 9
+        rh_L_plating_labor = 45
+        g_L_plating_labor = 50
+        rs_L_plating_labor = 48
+        L_e_coating_labor = 4
+        L_mark_up = 12
     
     
   
